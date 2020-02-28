13c3c5472a2046ab17046ee6a29e8e9354525dff
YuChia-Wei <h42279@gmail.com>
2020-02-28 15:47:20 +0800

    重新調整擴充方法位置，統一到 LinqExtensions

--- CSharpAdvanceDesignTests/LinqExtensions.cs
+++ CSharpAdvanceDesignTests/LinqExtensions.cs
@@ -3,8 +3,26 @@ using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
-    public static class CustomWhereExtensions
+    public static partial class LinqExtensions
     {
+        public static IEnumerable<TResult> JoeySelect<TSource, TResult>(this IEnumerable<TSource> sources, Func<TSource, TResult> selector)
+        {
+            foreach (var employee in sources)
+            {
+                yield return selector(employee);
+            }
+        }
+
+        public static IEnumerable<TResult> JoeySelect<TSource,TResult>(this IEnumerable<TSource> sources, Func<TSource, int, TResult> joeySelectWithIndex)
+        {
+            var index = 0;
+            foreach (var source in sources)
+            {
+                yield return joeySelectWithIndex(source, index);
+                index++;
+            }
+        }
+
         public static List<TSource> JoeyWhere<TSource>(this List<TSource> sources, Func<TSource, bool> validFunc)
         {
             var result = new List<TSource>();
