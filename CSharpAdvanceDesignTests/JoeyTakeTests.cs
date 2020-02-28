1b230623cc08522d648d35ebb8440a582fd86c57
YuChia-Wei <h42279@gmail.com>
2020-02-28 16:34:32 +0800

    加入 take 3 的測試

--- CSharpAdvanceDesignTests/JoeyTakeTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeTests.cs
@@ -14,12 +14,36 @@ namespace CSharpAdvanceDesignTests
         {
             var employees = GetEmployees();
 
-            var actual = JoeyTake(employees);
+            var actual = JoeyTake(employees, 2);
 
             var expected = new List<Employee>
             {
                 new Employee {FirstName = "Joey", LastName = "Chen"},
                 new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void take_3_employees()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Mike", LastName = "Chang"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"},
+            };
+
+            var actual = JoeyTake(employees, 3);
+
+            var expected = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"},
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);
@@ -37,11 +61,10 @@ namespace CSharpAdvanceDesignTests
             };
         }
 
-        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
+        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees, int count)
         {
             var enumerator = employees.GetEnumerator();
             var index = 0;
-            var count = 2;
             while (enumerator.MoveNext())
             {
                 if (index < count)
