5858323e8bfd3e41de4307be9d2f215779f6cd50
YuChia <h42279@gmail.com>
2020-02-29 13:51:11 +0800

    Add function by 91

--- CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
@@ -27,6 +28,23 @@ namespace CSharpAdvanceDesignTests
         }
 
         [Test]
+        public void get_last_employee_last_name_is_Chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"},
+            };
+
+            var employee = JoeyLastOrDefault(employees, current => current?.LastName == "Chen");
+
+            new Employee { FirstName = "David", LastName = "Chen" }
+            .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
         public void get_null_when_employees_is_empty()
         {
             var employees = new List<Employee>();
@@ -34,6 +52,11 @@ namespace CSharpAdvanceDesignTests
             Assert.IsNull(actual);
         }
 
+        /// <summary>
+        /// by YuChia
+        /// </summary>
+        /// <param name="employees"></param>
+        /// <returns></returns>
         private Employee JoeyLastOrDefault(IEnumerable<Employee> employees)
         {
             var enumerator = employees.GetEnumerator();
@@ -46,6 +69,45 @@ namespace CSharpAdvanceDesignTests
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// by 91
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private TSource JoeyLastOrDefault<TSource>(IEnumerable<TSource> source)
+        {
+            var enumerator = source.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                return default(TSource);
+            }
+
+            var last = enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                last = enumerator.Current;
+            }
+
+            return last;
+        }
+
+        private TSource JoeyLastOrDefault<TSource>(IEnumerable<TSource> sources, Func<TSource, bool> predicate)
+        {
+            var enumerator = sources.GetEnumerator();
+            TSource employee = default(TSource);
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (predicate(current))
+                {
+                    employee = current;
+                }
+            }
+
+            return employee;
         }
     }
 }
