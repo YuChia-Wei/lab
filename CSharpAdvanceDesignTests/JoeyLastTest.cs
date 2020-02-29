f76163847295c0dd241a00cc9b7453c914decef5
YuChia <h42279@gmail.com>
2020-02-29 11:48:10 +0800

    Finish JoeyLast

---
+++ CSharpAdvanceDesignTests/JoeyLastTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpectedObjects;
+using Lab.Entities;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class JoeyLastTest
+    {
+        [Test]
+        public void get_last_employee()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"},
+            };
+
+            var employee = JoeyLast(employees);
+
+            new Employee { FirstName = "Cash", LastName = "Li" }
+                .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
+        public void get_last_employee_when_no_employee()
+        {
+            var employees = new Employee[]
+            {
+            };
+
+            TestDelegate action = () => JoeyLast(employees);
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        private Employee JoeyLast(IEnumerable<Employee> employees)
+        {
+            var enumerator = employees.GetEnumerator();
+
+            Employee current = null;
+            while (enumerator.MoveNext())
+            {
+                current = enumerator.Current;
+            }
+
+            return current ?? throw new InvalidOperationException($"{nameof(employees)} is Empty.");
+        }
+    }
+}
