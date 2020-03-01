6c6c4f87de92681028ec612d9f76182fdbaad0ba
YuChia <h42279@gmail.com>
2020-03-01 15:28:47 +0800

    [Except] - Add new test case :except_numbers_tran

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -19,6 +19,18 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void except_numbers_tran()
+        {
+            var first = new[] { 1, 3, 5, 7, 3 };
+            var second = new[] { 7, 1, 4, 1 };
+
+            var actual = JoeyExcept(second, first);
+            var expected = new[] { 4 };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
             var secondHashSet = new HashSet<int>(second);
