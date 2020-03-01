b545b2b4cf299e0cc5bbf2b69116ef042c678fb5
YuChia <h42279@gmail.com>
2020-03-01 11:20:23 +0800

    將 Sort 方法移動到 MyOrderEnumerable

--- Lab/LinqExtensions.cs
+++ Lab/LinqExtensions.cs
@@ -193,53 +193,5 @@ namespace CSharpAdvanceDesignTests
         {
             return employees;
         }
-
-        public static IEnumerable<Employee> JoeySort(this IEnumerable<Employee> employees,
-            IComparer<Employee> comboComparer)
-        {
-            //Selection sort
-            var elements = employees.ToList();
-            while (elements.Any())
-            {
-                var minElement = elements[0];
-                var index = 0;
-                for (int i = 1; i < elements.Count; i++)
-                {
-                    var employee = elements[i];
-
-                    if (comboComparer.Compare(employee, minElement) < 0)
-                    {
-                        minElement = employee;
-                        index = i;
-                    }
-                }
-
-                elements.RemoveAt(index);
-                yield return minElement;
-            }
-        }
-
-        public static IEnumerable<Employee> JoeySort(this IEnumerable<Employee> employees)
-        {
-            //Selection sort
-            var stringComparer = Comparer<string>.Default;
-            var elements = employees.ToList();
-            while (elements.Any())
-            {
-                var minElement = elements[0];
-                var index = 0;
-                for (int i = 1; i < elements.Count; i++)
-                {
-                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
-                    {
-                        minElement = elements[i];
-                        index = i;
-                    }
-                }
-
-                elements.RemoveAt(index);
-                yield return minElement;
-            }
-        }
     }
 }
