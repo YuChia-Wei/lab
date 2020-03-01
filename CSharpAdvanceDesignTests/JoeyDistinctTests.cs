e0ddb23cb89ec2261d36a1fcb2c38e29e31a3f35
YuChia <h42279@gmail.com>
2020-03-01 13:38:02 +0800

    加入 distinct_employees 測試

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using ExpectedObjects;
+using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -17,6 +18,34 @@ namespace CSharpAdvanceDesignTests
             var expected = new[] { 91, 3, -1 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinct(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinct(IEnumerable<Employee> employees)
+        {
+            throw new System.NotImplementedException();
         }
 
         private IEnumerable<int> Distinct(IEnumerable<int> numbers)
