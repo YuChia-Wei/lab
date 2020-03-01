b3cef8086779f5ef3f599782fadd8e03ba97456e
YuChia <h42279@gmail.com>
2020-03-01 10:00:47 +0800

    將 JoeyOrderByLastName 改為 JoeySory & 調整為擴充方法

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -21,7 +21,7 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
 
-            var actual = JoeyOrderByLastName(employees);
+            var actual = employees.JoeySort();
 
             var expected = new[]
             {
@@ -88,29 +88,6 @@ namespace CSharpAdvanceDesignTests
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);
-        }
-
-        private IEnumerable<Employee> JoeyOrderByLastName(IEnumerable<Employee> employees)
-        {
-            //Selection sort
-            var stringComparer = Comparer<string>.Default;
-            var elements = employees.ToList();
-            while (elements.Any())
-            {
-                var minElement = elements[0];
-                var index = 0;
-                for (int i = 1; i < elements.Count; i++)
-                {
-                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
-                    {
-                        minElement = elements[i];
-                        index = i;
-                    }
-                }
-
-                elements.RemoveAt(index);
-                yield return minElement;
-            }
         }
     }
 }
--- Lab/LinqExtensions.cs
+++ Lab/LinqExtensions.cs
@@ -15,7 +15,7 @@ namespace CSharpAdvanceDesignTests
             }
         }
 
-        public static IEnumerable<TResult> JoeySelect<TSource,TResult>(this IEnumerable<TSource> sources, Func<TSource, int, TResult> joeySelectWithIndex)
+        public static IEnumerable<TResult> JoeySelect<TSource, TResult>(this IEnumerable<TSource> sources, Func<TSource, int, TResult> joeySelectWithIndex)
         {
             var index = 0;
             foreach (var source in sources)
@@ -198,6 +198,29 @@ namespace CSharpAdvanceDesignTests
                     if (comboComparer.Compare(employee, minElement) < 0)
                     {
                         minElement = employee;
+                        index = i;
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+
+        public static IEnumerable<Employee> JoeySort(this IEnumerable<Employee> employees)
+        {
+            //Selection sort
+            var stringComparer = Comparer<string>.Default;
+            var elements = employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
+                    {
+                        minElement = elements[i];
                         index = i;
                     }
                 }
